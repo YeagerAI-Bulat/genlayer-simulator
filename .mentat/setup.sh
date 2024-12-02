8ea05b83e5d550716c64ca610a8ae5c0f179d4c4
MentatBot <160964065+MentatBot@users.noreply.github.com>
2024-12-02 11:47:54 +0000

    Fix Mentat Scripts: npm --prefix frontend run buil...
    Error output:
    =====15 Lines Omitted=====
    
    ==============================
    Full log:[here](https://mentat.ai/log/2ecd9e44-bf01-43fc-81d9-77c8ab3583d7)

--- .mentat/setup.sh
+++ .mentat/setup.sh
@@ -5,5 +5,7 @@ python3 -m venv .venv
 pip3 install -r requirements.txt
 pip3 install -r backend/database_handler/migration/requirements.txt
 [ -f .env ] || cp .env.example .env
+mkdir -p frontend/src/assets/examples
+cp -r examples/* frontend/src/assets/examples/
 npm --prefix frontend install
 npm --prefix frontend run build
