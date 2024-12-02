dfec134494daa0dcbc7bf7161c3c7517c99473c8
MentatBot <160964065+MentatBot@users.noreply.github.com>
2024-12-02 11:52:15 +0000

    Fix Mentat Scripts: pytest
    Error output:
    =====2 Lines Omitted=====
    
    ==============================
    Full log:[here](https://mentat.ai/log/e094492f-6279-452b-ab9c-6916b9fcb5c5)

--- .mentat/setup.sh
+++ .mentat/setup.sh
@@ -4,6 +4,7 @@ python3 -m venv .venv
 . .venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install -r backend/database_handler/migration/requirements.txt
+pip3 install -e backend/protocol_rpc
 [ -f .env ] || cp .env.example .env
 mkdir -p frontend/src/assets/examples
 cp -r examples/* frontend/src/assets/examples/
